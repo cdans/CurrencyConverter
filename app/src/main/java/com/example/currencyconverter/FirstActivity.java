d3cbbc551629fdb847469e5c051fd4fd3d56f2d5
cdans <dietlof22>
2019-10-01 00:37:23 +0300

    End of 30.9.2019

--- app/src/main/java/com/example/currencyconverter/FirstActivity.java
+++ app/src/main/java/com/example/currencyconverter/FirstActivity.java
@@ -4,18 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     Button _button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.firstactivitydesign);
 
         //Our button
         _button = (Button) findViewById(R.id.button);
-        _button.setOnClickListener(new View.OnClickListener();
+        _button.setOnClickListener(new View.OnClickListener(){
 
         @Override
                 public void onClick(View view) {
--- app/src/main/java/com/example/currencyconverter/SecondActivity.java
+++ app/src/main/java/com/example/currencyconverter/SecondActivity.java
@@ -1,3 +1,5 @@
+package com.example.currencyconverter;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.currencyconverter.R;
 
-public class FirstActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     private Button myButton;
     private TextView mytextField;
@@ -15,7 +17,7 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.firstactivitydesign);
+        setContentView(R.layout.secondactivitydesign);
 
         myButton = (Button) findViewById(R.id.button);
         mytextField = (TextView) findViewById(R.id.textfield);
@@ -24,9 +26,9 @@ public class FirstActivity extends AppCompatActivity {
     }
 
         View.OnClickListener myButtonClick = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mytextField.setText(mytextField.getText()+"n" + "Next line");
-            }
-        };
-    }
+                @Override
+                public void onClick (View v){
+                    mytextField.setText(mytextField.getText() + "\n" + "Next line");
+                }
+            };
+        }
